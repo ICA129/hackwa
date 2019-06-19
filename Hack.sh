96e7076bb058932f9d62dd6043510f1202595b75
ICA129 <51882241+ICA129@users.noreply.github.com>
2019-06-19 18:32:29 +0700

    Update Hack.sh

--- Hack.sh
+++ Hack.sh
@@ -8,21 +8,24 @@ echo "
 ####################
 Copyright (c) 2018 B4NC1_P3RK454";
 
-echo "\e[0;34m TOOLS INI DIBUAT OLEH B4NC1_P3RK454"]
+echo "\034[1;32m TOOLS INI DIBUAT OLEH B4NC1_P3RK454.."]
 sleep 3
+echo "\033[1;32m PERHATIAN JIKA TOOLS INI TIDAK BERFUNGSI BERARTI ANDA BUKAN BANCI..."]
+sleep 3
+echo "033[1;32m SYARAT MENGGUNAKAN TOOLS INI ADALAH MENJADI BANCI.."]
 echo "";
 read -p "Nomer Kamu (+62xxxx)=> " nomer;
 read -p "Nomer Target (+62xxxx) => " target;
 sleep 2
 echo "[+] Syncing Target...";
 sleep 5
-echo "TIDAK BISA TERCONECT OLEH TARGET" ;
+echo "\033[1;34m TIDAK BISA TERHUBUNG  OLEH TARGET..."] ;
 sleep 2
 
 
-echo "MENCOBA MENELPON TARGET" ;
+echo "\033[1;36m MENCOBA MENELPON TARGET.."] ;
 
-echo "MOHON TUNGGU"
+echo "\033[1;36m MOHON TUNGGU..."]
 
 pkg install php -y
 
