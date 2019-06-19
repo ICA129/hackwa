2eea2bc226deaa15e3969fd9d5d8e1b22a559791
ICA129 <51882241+ICA129@users.noreply.github.com>
2019-06-19 17:02:36 +0700

    Create Hack.sh

---
+++ Hack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+echo "
+<-- IndoSec -->
+####################
+#   Whatsapp Code  # By ./B4NC1_PERK454
+#     Exploiter    # bancitamfan56@gmail.com
+####################
+Copyright (c) 2018 B4NC1_P3RK454";
+echo "";
+read -p "Nomer Kamu (+62xxxx)=> " nomer;
+read -p "Nomer Target (+62xxxx) => " target;
+sleep 2
+echo "[+] Syncing Target...";
+sleep 1
+echo "TIDAK BISA TERCONECT OLEH TARGET" ;
+sleep 2
+echo "MENCOBA MENELPON TARGET" ;
+sleep 2
+echo "SEDANG MELAKUKAN INSTALLING" ;
+system "pkg install php -y";
+system "git clone https://github.com/ICA129/spamcall" ;
+system " cd spamcall" ;
+system " php SpamCall.php";
